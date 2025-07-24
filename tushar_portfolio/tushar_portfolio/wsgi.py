01b20c2b2f7b8c1b1a743a857064708096ebe6ec
tushar-kumar-9354 <jangratushar@348gmail.com>
2025-07-24 12:34:30 +0530

    done

--- tushar_portfolio/tushar_portfolio/wsgi.py
+++ tushar_portfolio/tushar_portfolio/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 print("THIS IS MY WSGI.PY folder 00")
 import os
-
+print("THIS IS MY WSGI.PY folder 01 and error comes after this line")
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tushar_portfolio.settings')
